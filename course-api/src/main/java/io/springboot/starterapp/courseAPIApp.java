f88f95d08b825fbccf33f5d8418bd5d77ec5d5b6
anikate-singhal <54211803+anikate-singhal@users.noreply.github.com>
2019-08-17 20:10:38 +0530

    New

---
+++ course-api/src/main/java/io/springboot/starterapp/courseAPIApp.java
@@ -0,0 +1,14 @@
+package io.springboot.starterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseAPIApp {
+	public static void main(String args[])
+	{
+		SpringApplication.run(courseAPIApp.class, args);
+		
+	}
+
+}
